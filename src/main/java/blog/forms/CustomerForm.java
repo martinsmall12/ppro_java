c2e0d2cd2506a022c8c3a6ef9b8a816a60363a31
Martin Brendl <mabrendl@gmail.com>
2017-01-14 17:17:15 +0100

    Add CustomerForm

---
+++ src/main/java/blog/forms/CustomerForm.java
@@ -0,0 +1,116 @@
+package blog.forms;
+
+
+import javax.validation.constraints.Size;
+
+public class CustomerForm {
+    @Size(max = 30)
+    private String ico;
+
+    @Size(max = 30)
+    private String dic;
+
+    @Size(max = 100)
+    private String company;
+
+    @Size(max = 100)
+    private String name;
+
+    @Size(max = 100)
+    private String street;
+
+    @Size(max = 100)
+    private String city;
+
+    @Size(max = 100)
+    private String state;
+
+    @Size(max = 5)
+    private String psc;
+
+    @Size(max = 100)
+    private String email;
+
+    @Size(max = 20)
+    private String phone;
+
+    public String getIco() {
+        return ico;
+    }
+
+    public void setIco(String ico) {
+        this.ico = ico;
+    }
+
+    public String getDic() {
+        return dic;
+    }
+
+    public void setDic(String dic) {
+        this.dic = dic;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPsc() {
+        return psc;
+    }
+
+    public void setPsc(String psc) {
+        this.psc = psc;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
